d32d5bab62b86390a940cd50282f18f9b45f1e41
Stephen Doty <stephendoty826@yahoo.com>
2021-10-04 09:20:13 -0500

    blackjack game

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,55 @@
 
 let deck = []
+let deck2 = []
+let playerHand = []
+var dealerHand = []
 let cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+let cardNumbers2 = [1, 11, 12, 13]
 let suits = ["clubs", "diamonds", "hearts", "spades"]
 let deal = document.querySelector("#deal-button")
 let hit = document.querySelector("#hit-button")
 let stand = document.querySelector("#stand-button")
-let dealerHand = document.querySelector("#dealer-hand")
-let playerHand = document.querySelector("#player-hand")
+// let playAgain = document.querySelector("#play-again-button")
+let dealer = document.querySelector("#dealer")
+let player = document.querySelector("#player")
+let message = document.querySelector("#message")
 
 // create deck programmatically
-cardNumbers.forEach(num => {
-  suits.forEach(suit => {
-    let card = {number: num, suit: suit, src: `images/${num}_of_${suit}.png`}
-    deck.push(card)
+function buildDeck(deck){
+  cardNumbers.forEach(num => {
+    suits.forEach(suit => {
+      let card = {}
+      if(num > 1 && num <=10){
+        card = {number: num, suit: suit, points: num, src: `images/${num}_of_${suit}.png`}
+      }
+      else if(num == 1){
+        card = {number: num, suit: suit, points: 11, src: `images/${num}_of_${suit}.png`}
+      }
+      else{
+        card = {number: num, suit: suit, points: 10, src: `images/${num}_of_${suit}.png`}
+      }
+      deck.push(card)
+    })
   })
-})
+}
+
+function buildDeck2(deck){
+  cardNumbers2.forEach(num => {
+    suits.forEach(suit => {
+      let card = {}
+      if(num == 1){
+        card = {number: num, suit: suit, points: 11, src: `images/${num}_of_${suit}.png`}
+      }
+      else if(num == 11 || num == 12 || num == 13){
+        card = {number: num, suit: suit, points: 10, src: `images/${num}_of_${suit}.png`}
+      }
+      deck.push(card)
+    })
+  })
+}
 
 // function to shuffle the deck array
-function shuffleDeck() {
+function shuffleDeck(deck) {
   for (var i = deck.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
       var temp = deck[i];
@@ -26,43 +58,134 @@ function shuffleDeck() {
   }
 }
 
-//create dealCard function which uses .pop() on the deck to deal 1 card to the player, 1 card to the dealer, then another card to the player, and a face-down card to the dealer. 
+//create dealCardTo function which uses .pop() on the deck to deal 1 card to the player, 1 card to the dealer, then another card to the player, and a face-down card to the dealer. 
 
-function dealCard(deck, hand){
+function dealCardTo(person, deck){
   let card = deck.pop()
-  let newCard = document.createElement("img")
+  let newCard = document.createElement("img") 
   newCard.setAttribute("class", "card1")
   newCard.setAttribute("src", `${card.src}`)
-  hand.append(newCard) 
+  person.children[1].append(newCard) // appends card to person's hand
+  let points = parseInt(person.children[0].children[0].textContent) // converts "current points" to int and sets equal to points
+  points += card.points // adds card points to variable points
+  person.children[0].children[0].textContent = points.toString() // sets "current points" to string value of points
+  if(person.id == "player"){
+    playerHand.push(card.number)
+    console.log(`playerHand = ${playerHand}`);
+  }
+  if(person.id == "dealer"){
+    dealerHand.push(card.number)
+    console.log(`dealerHand = ${dealerHand}`)
+  }
 }
 
-shuffleDeck()
+function displayMessage(messageText){
+  message.textContent = messageText
+}
 
+function aceInHand(person, personHand){
+  let points = parseInt(person.children[0].children[0].textContent) // converts "current points" to int and sets equal to points
+  points -= 10 // converts ace to value of 1 point
+  person.children[0].children[0].textContent = points.toString() // sets "current points" to string value of points
+  let index = personHand.indexOf(1)
+  if(index > -1){
+    personHand.splice(index, 1)
+  }
+  else{
+    alert(`aceInHand() was run but no ace was found in ${person.id}'s hand`)
+  }
+}
+
+buildDeck(deck)
+
+shuffleDeck(deck)
 console.log(deck)
 
+buildDeck2(deck2)
+
+shuffleDeck(deck2)
+console.log(deck2)
+
+displayMessage("Click \"Deal\" to play")
+
 deal.addEventListener("click", () => {
-  dealCard(deck, playerHand)
-
-  dealCard(deck, dealerHand)
-
-  dealCard(deck, playerHand)
-
-  dealCard(deck, dealerHand)
+  if(dealerHand.length === 0){
+    dealCardTo(player, deck)
+    dealCardTo(dealer, deck)
+    dealCardTo(player, deck)
+    dealCardTo(dealer, deck)
+    if(dealer.children[0].children[0].textContent == "22"){
+      aceInHand(dealer, dealerHand)
+    }
+    if(player.children[0].children[0].textContent == "22"){
+      aceInHand(player, playerHand)
+    }
+    if(dealer.children[0].children[0].textContent == "21" && player.children[0].children[0].textContent == "21"){
+      displayMessage("PUSH...you don't win and you don't lose.")
+    }
+    else if(dealer.children[0].children[0].textContent == "21"){
+      displayMessage("House wins. Please play again.")
+    }
+    else if(player.children[0].children[0].textContent == "21"){
+      displayMessage("Perfect Hand. You win!")
+    }
+  }
 })
 
 hit.addEventListener("click", () => {
-  dealCard(deck, playerHand)
+  if(dealerHand.length != 0){
+    if(parseInt(player.children[0].children[0].textContent) < 21){
+      dealCardTo(player, deck)
+    }
+    if(parseInt(player.children[0].children[0].textContent) > 21 && playerHand.includes(1)){
+      aceInHand(player, playerHand)
+    }
+    if(parseInt(player.children[0].children[0].textContent) > 21){
+      displayMessage("Player bust. House wins.")
+    }
+  }
 })
 
 stand.addEventListener("click", () => {
+  if(dealerHand.length != 0){
+    while (parseInt(dealer.children[0].children[0].textContent) <= 16){ // 
+      dealCardTo(dealer, deck)
+      if(parseInt(dealer.children[0].children[0].textContent) > 21 && dealerHand.includes(1)){
+        aceInHand(dealer, dealerHand)
+      }
+    }
+    if(parseInt(dealer.children[0].children[0].textContent) <= 21){
+      if(parseInt(dealer.children[0].children[0].textContent) > parseInt(player.children[0].children[0].textContent)){
+        displayMessage("House wins. Please play again.")
+      }
+      else if(parseInt(dealer.children[0].children[0].textContent) === parseInt(player.children[0].children[0].textContent)){
+        displayMessage("PUSH...you don't win and you don't lose.")
+      }
+      else{
+        displayMessage("You win. Congratulations!!!")
+      }
+    }
+    else{
+      displayMessage("Dealer bust. You win.")
+    }
+  }
 })
 
+playAgain.addEventListener("click", () => {
+  deck = []
+  buildDeck()
+  shuffleDeck()
+  playerHand = []
+  dealerHand = []
+  // make points = 0
+  // remove divs from dealer and player hands
+})
 
-// add up value of cards and put current score in place
+// todo if player score goes over 21, print out "BUST!" in message area
 
-// if player score goes over 21, print out "BUST!" in message area
+// todo create function (playerStands())to deal cards to dealer after player hits stand
 
-// create function (playerStands())to deal cards to dealer after player hits stand
+// todo calculate value. If dealer or player go over 21, they lose.
 
 
-// calculate value. If dealer or player go over 21, they lose.
+// todo have game logic function???
--- scripts/test.js
+++ scripts/test.js
@@ -1,24 +1,39 @@
 
 
-function shuffleArray(array){
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1))
-        var temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
-    }
+// function shuffleArray(array){
+//     for (var i = array.length - 1; i > 0; i--) {
+//         var j = Math.floor(Math.random() * (i + 1))
+//         var temp = array[i]
+//         array[i] = array[j]
+//         array[j] = temp
+//     }
+// }
+
+
+// // arr.length = 5
+
+// // i = 4
+
+// array = [1, 2, 3, 4, 5]
+
+// for (var i = array.length - 1; i > 0; i--) { // i = 4 
+//     var j = Math.floor(Math.random() * (i + 1)) // j = some random number (Math.floor(Math.random() * (4 + 1))) let's assume j = 2
+//     var temp = array[i] // temp = 5
+//     array[i] = array[j] // array[4] = 3 (array[2])
+//     array[j] = temp // array[2] = 5
+// }
+
+arr = [1, 4, 6, 7, 9, 3]
+
+let index = arr.indexOf(1)
+
+console.log(arr)
+console.log(`index = ${index}`)
+
+if(index > -1){
+    arr.splice(index, 1)
 }
 
+console.log(arr);
 
-// arr.length = 5
 
-// i = 4
-
-array = [1, 2, 3, 4, 5]
-
-for (var i = array.length - 1; i > 0; i--) { // i = 4 
-    var j = Math.floor(Math.random() * (i + 1)) // j = some random number (Math.floor(Math.random() * (4 + 1))) let's assume j = 2
-    var temp = array[i] // temp = 5
-    array[i] = array[j] // array[4] = 3 (array[2])
-    array[j] = temp // array[2] = 5
-}
